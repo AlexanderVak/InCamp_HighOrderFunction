ca196f73c60fb2cc34b49cf72953c50aa61bf623
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-26 18:15:56 +0300

    add functional wrappers for map, filter, reduce
    flow unfinnished

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-import { reduce } from "lodash";
+import { curry, flow } from "lodash"
 
 export const and = (p1, p2) => x => p1(x) && p2(x)
 export const all = predicates => predicates.reduce(and, () => true)
 export const any = predicates => predicates.reduce(or)
 export const or = (p1, p2) => x => p1(x) || p2(x)
 
-let hasColor = color => figure => figure.color === color
+let hasColor = curry((color, rectangle) => rectangle.color === color)
 
 export const isRed = hasColor('red')
 export const isBlack = hasColor('black')
 export const isWhite = hasColor('white')
 
-export const isSquare = figure => figure.height === figure.width
+                        //callbackFn => items => items.map(callbackFn)
+export const map = curry((callbackFn, items) => items.map(callbackFn))  
+export const filter = curry((callbackFn, items) => items.filter(callbackFn))
+export const reduce = (callbackFn, initialValue) => items => items.reduce(callbackFn, initialValue)
+
+export const isSquare = rectangle => rectangle.height === rectangle.width
+const calcArea = rectangle => rectangle.width * rectangle.height
+
+export let calcBlackSquareArea = flow(
+    filterter(and(isSquare, isBlack)),
+    map(calcArea)
+)
+
+
+
 
 
 
--- index.spec.js
+++ index.spec.js
@@ -1,42 +1,50 @@
-import { isBlack, isRed, isWhite, or, and, isSquare, any, all } from '.';
+import { isBlack, isRed, isWhite, or, and, isSquare, any, all, filter, calcBlackSquareArea } from '.';
 import rectangles from './figures'
 
 describe('All figures', () => {
     it('should return figures with red color', () => {
-        expect(rectangles.filter(isRed)).toEqual([
+        expect(filter(isRed, rectangles)).toEqual([
             rectangles[0], rectangles[3]
         ])
     });
     it('should return figures with black color', () => {
-        expect(rectangles.filter(isBlack)).toEqual([
+        expect(filter(isBlack, rectangles)).toEqual([
             rectangles[1], rectangles[4]
         ]);
     });
 
     it('should return figures with white color', () => {
-        expect(rectangles.filter(isWhite)).toEqual([
+        expect(filter(isWhite, rectangles)).toEqual([
             rectangles[2], rectangles[5]
         ]);
     });
 });
 describe('Predicates', () => {
     it('should return red or black figures', () => {
-        expect(rectangles.filter(or(isRed, isBlack))).toEqual([
+        expect(filter(or(isRed, isBlack), rectangles)).toEqual([
             rectangles[0], rectangles[1], rectangles[3], rectangles[4],
         ]);
     });
 
     it('should return red square', () => {
-        expect(rectangles.filter(and(isRed, isSquare))).toEqual([rectangles[0]]);
+        expect(filter(and(isRed, isSquare), rectangles)).toEqual([rectangles[0]]);
     });
 
     it('should return squares or white figures or figures with height > 4', () => {
-        expect(rectangles.filter(any([isWhite, isSquare, (figure) => figure.height > 4]))).toEqual([
+        expect(filter(any([isWhite, isSquare, (figure) => figure.height > 4]), rectangles)).toEqual([
             rectangles[0], rectangles[1], rectangles[2], rectangles[5],
         ]);
     });
 
     it('should return white square with height > 2', () => {
-        expect(rectangles.filter(all([isWhite, isSquare, (figure) => figure.height > 2]))).toEqual([rectangles[2]]);
+        expect(filter(all([isWhite, isSquare, (figure) => figure.height > 2]), rectangles)).toEqual([rectangles[2]]);
+    });
+});
+
+describe('Flow functions', () => {
+    it('should return array of areas', () => {
+        expect(calcBlackSquareArea(rectangles)).toEqual([
+            100, 40
+        ]);
     });
 });
