ba36c73c6c548775d450a0389eaa1e3458c5117e
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-26 15:43:38 +0300

    add figures array

---
+++ figures.js
@@ -0,0 +1,34 @@
+const rectangles = [
+    {
+        color: 'red',
+        width: 5,
+        height: 5,
+    },
+    {
+        color: 'black',
+        width: 10,
+        height: 10,
+    },
+    {
+        color: 'white',
+        width: 7,
+        height: 7,
+    },
+    {
+        color: 'red',
+        width: 8,
+        height: 2,
+    },
+    {
+        color: 'black',
+        width: 10,
+        height: 4,
+    },
+    {
+        color: 'white',
+        width: 15,
+        height: 8,
+    }
+
+]
+export default rectangles
